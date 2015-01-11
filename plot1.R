4993e6ae08ffdb978b9c1f33d4400277eb11cc89
Ryan Meyer <ryan1234@gmail.com>
2015-01-10 21:07:47 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,8 @@
 library(plyr)
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="data.zip", method="curl")
+unzip("data.zip")
 setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y"))
-df <- read.table("household.txt", header=TRUE, sep=";", na.strings = 'NULL', 
+df <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = 'NULL', 
                 colClasses=c("myDate","character","character","character","character","character","character","character","character"))
 png(filename="plot1.png", width=480, height=480)
 with(subset(df, Date == "2007-02-01" | Date == "2007-02-02"), 
